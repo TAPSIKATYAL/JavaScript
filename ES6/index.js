e3ab4890bad4d8ba6c403c670c157f9f26ea3d21
TAPSIKATYAL <tapsi.katyal123.tk@gmail.com>
2024-06-08 12:22:02 +0530

    implemented rest operator

--- ES6/index.js
+++ ES6/index.js
@@ -19,4 +19,21 @@ const job = {
 const personWithJob = {...person,...job};
 console.log(personWithJob);
 //what is the rest operator
-// The rest operator is used to collect mutiple elements/ properties into single array or object.
+// The rest operator is used to collect mutiple elements/ properties into single array or object. It is(...) used in function parameters to accept indefinite number of arguments
+
+
+function sum(...numbers){
+return numbers.reduce((total,num)=> total+num,0);
+}
+
+console.log(sum(1,2,3));
+console.log(sum(1,2,3,4,5,6));
+
+//Used in object restructing as well
+//You can use the rest operator to collect remaining properties of an object when destructuring.
+const person1 = {name:'sneha', age:'23', job:'Hr', salary:'35999'};
+
+const {name,age, ...rest} = person1;
+console.log(name);
+console.log(age);
+console.log(rest);
