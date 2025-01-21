b0d64e4e10d6b01ded362a53a963ce4ef682fe72
Tapsi Katyal <tapsi.katyal123.tk@gmail.com>
2025-01-21 11:10:02 +0530

    added explanation

--- InterviewQuestion/sumOfNArguements.js
+++ InterviewQuestion/sumOfNArguements.js
@@ -11,3 +11,9 @@ function sum(x){
 console.log(sum(2));
 
 console.log(sum(2)(3)(4)(7)());
+
+//sum(2) currentSum = 2 and return innerSum;
+//innerSum(3)  y=3 update currentSum = 5 return innerSum; // here sum will make a closure with innerSum
+//innerSum(4) y = 4 update currentSum = 9 return innerSum;
+//innerSum(7) y = 7 update currentSum = 9 return innerSum;
+//innerSum() y= undefined go to if condition and return currentSum;
